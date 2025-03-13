98c89ea3f52eddc2eecde59f468ccfd678e68971
Takanori Aki <aki@egrowth-inc.com>
2025-03-14 07:53:11 +0900

    Add ScrollToTop component to main rendering for improved navigation experience

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from '/src/App.tsx';
 import { LanguageProvider } from '/src/contexts/LanguageContext';
 import '/src/index.css';
+import ScrollToTop from '/src/components/ScrollToTop';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <LanguageProvider>
         <App />
       </LanguageProvider>
